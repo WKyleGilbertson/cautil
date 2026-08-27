e87750ab9e9ea0803c3c68a8a177793b8edf1ce6
W. Kyle Gilbertson <wkyleg@gmail.com>
2026-08-27 14:10:08 -0600

    Cleaned up Warnings thrown by gcc

--- cautil.cpp
+++ cautil.cpp
@@ -2,14 +2,13 @@
 #include <cstring>
 #include <cstdint>
 #include <cstdlib>
-//#include "G2DELAY.h"
 #include "G2INIT.h"
 #include "version.h"
 
 typedef struct
 {
   FILE *IN, *OUT;
-  uint8_t PRN;  //
+  uint8_t PRN;
   uint16_t phi; // Code Phase
   SWV V;
   bool showCode;
@@ -24,15 +23,15 @@ void initconfig(CONFIG *cfg)
 
 void processArgs(int argc, char *argv[], CONFIG *cfg)
 {
-  static int len, i, ch = ' ';
+  static int i, ch = ' ';
   static char usage[] =
       "cautil: CA Code Utility\n"
       "usage: cautil <PRN> [phase] [options]\n"
-      "       PRN             which PRN\n"
-      "       phase           code phase\n"
-      "       -s              show the CA Code\n"
-      "       -v              print version information\n"
-      "       -?|h            print this usage message\n"
+      "       PRN         which PRN\n"
+      "       phase       code phase\n"
+      "       -s          show the CA Code\n"
+      "       -v          print version information\n"
+      "       -?|h        print this usage message\n"
       "  defaults: PRN=135 phase=0\n";
 
   if (argc < 2)
@@ -89,34 +88,37 @@ int main(int argc, char *argv[])
   CONFIG cnfg;
   uint16_t ph;
 
-cnfg.V.Major = MAJOR_VERSION;
-cnfg.V.Minor = MINOR_VERSION;
-cnfg.V.Patch = PATCH_VERSION;
+  cnfg.V.Major = MAJOR_VERSION;
+  cnfg.V.Minor = MINOR_VERSION;
+  cnfg.V.Patch = PATCH_VERSION;
+  
 #ifdef CURRENT_HASH
-  strncpy(cnfg.V.GitCI, CURRENT_HASH, 40);
+  std::memcpy(cnfg.V.GitCI, CURRENT_HASH, sizeof(cnfg.V.GitCI));
+  cnfg.V.GitCI[sizeof(cnfg.V.GitCI) - 1] = '\0';
 #endif
 #ifdef CURRENT_DATE
-  strncpy(cnfg.V.BuildDate, CURRENT_DATE, 17);
+  std::memcpy(cnfg.V.BuildDate, CURRENT_DATE, sizeof(cnfg.V.BuildDate));
+  cnfg.V.BuildDate[sizeof(cnfg.V.BuildDate) - 1] = '\0';
 #endif
 #ifdef CURRENT_NAME
-  strncpy(cnfg.V.Name, CURRENT_NAME, 10);
+  std::memcpy(cnfg.V.Name, CURRENT_NAME, sizeof(cnfg.V.Name));
+  cnfg.V.Name[sizeof(cnfg.V.Name) - 1] = '\0';
 #endif
 
   initconfig(&cnfg);
   processArgs(argc, argv, &cnfg);
-  //G2DELAY sv(cnfg.PRN, cnfg.phi);
+  
   G2INIT sv(cnfg.PRN, 0); // Don't shift it yet
-  //G2INIT sv(cnfg.PRN, cnfg.phi);
 
   if (cnfg.showCode == true) {
-  for (ph=0; ph < 1023; ph++) 
-    fprintf(stdout, "%.1X", sv.CACODE[ph]);
-  fprintf(stdout, "\n");
+    for (ph=0; ph < 1023; ph++) 
+      fprintf(stdout, "%.1X", sv.CACODE[ph]);
+    fprintf(stdout, "\n");
   }
 
   fprintf(stdout, "G1[%.4d]:o%.4o G2[%.4d]:o%.4o G1:0x%.3X G2:0x%.3X CA:%.1X\n",
    cnfg.phi, sv.G1a[cnfg.phi], cnfg.phi, sv.G2a[cnfg.phi],
    sv.G1a[cnfg.phi], sv.G2a[cnfg.phi], sv.CACODE[cnfg.phi]);
-  //printf("First 10: %.4oo\n", sv.F10);   // First 10 bits of CA Code
+   
   return 0;
 }
